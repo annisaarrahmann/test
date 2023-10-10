ca419cc5abf4810a348f1c62fc9a57230cb60a1a
Naufal Zhafran Latif <latifnaufal7@gmail.com>
2023-10-10 21:41:24 +0700

    fix data error

--- src/data.ts
+++ src/data.ts
@@ -1,122 +1,124 @@
 export const menu = [
-    {
+  {
+    id: 1,
+    title: "dashboard",
+    listItems: [
+      {
         id: 1,
-        title: "dashboard",
-        listItems: [
-            {
-                id: 1,
-                title: "Homepage",
-                url: "/",
-                icon: "home.svg",
-            },
-        ]
-    },
-    {
+        title: "Homepage",
+        url: "/",
+        icon: "home.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Kelola Akun",
+    listItems: [
+      {
+        id: 1,
+        title: "Users",
+        url: "/users",
+        icon: "user.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Transaksi",
+    listItems: [
+      {
+        id: 1,
+        title: "Surat",
+        url: "/",
+        icon: "note.svg",
+      },
+      {
         id: 2,
-        title: "Kelola Akun",
-        listItems: [
-            {
-                id: 1,
-                title: "Users",
-                url: "/users",
-                icon: "user.svg",
-            },
-        ]
-    },
-    {
-        id: 3,
-        title: "Transaksi",
-        listItems: [
-            {
-                id: 1,
-                title: "Surat",
-                url: "/",
-                icon: "note.svg",
-            },
-            {
-                id: 2,
-                title: "Forms",
-                url: "/",
-                icon: "form.svg",
-            },
-        ]
-    },
-    {
-        id: 4,
-        title: "Maintenance",
-        listItems: [
-            {
-                id: 1,
-                title: "Settings",
-                url: "/",
-                icon: "setting.svg",
-            },
-            {
-                id: 2,
-                title: "Log Out",
-                url: "/",
-                icon: "",
-            },
-        ]
-    },
-]
+        title: "Forms",
+        url: "/",
+        icon: "form.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Maintenance",
+    listItems: [
+      {
+        id: 1,
+        title: "Settings",
+        url: "/",
+        icon: "setting.svg",
+      },
+      {
+        id: 2,
+        title: "Log Out",
+        url: "/",
+        icon: "",
+      },
+    ],
+  },
+];
 
 export const userRows = [
-    {
-        id: 1,
-        img: 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
-        name: 'Tamie Astuti',
-        email: 'none@gmail.com',
-        department: 'Direktur Administrasi & Keuangan',
-    },
-    {
-        id: 2,
-        img: 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
-        name: 'Kusino',
-        email: 'none@gmail.com',
-        department: 'Manager Divisi Pengadaan'
-    },
-    {
-        id: 3,
-        img: 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
-        name: 'Novita',
-        email: 'none@gmail.com',
-        department: 'Tenaga Terampil Dep. Pengadaan'
-    },
+  {
+    id: 1,
+    img: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+    name: "Tamie Astuti",
+    email: "none@gmail.com",
+    department: "Direktur Administrasi & Keuangan",
+  },
+  {
+    id: 2,
+    img: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+    name: "Kusino",
+    email: "none@gmail.com",
+    department: "Manager Divisi Pengadaan",
+  },
+  {
+    id: 3,
+    img: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+    name: "Novita",
+    email: "none@gmail.com",
+    department: "Tenaga Terampil Dep. Pengadaan",
+  },
 ];
 
 export const products = [
-    {
-        id: 1,
-        img: 'https://asset-2.tstatic.net/lampung/foto/bank/images/ilustrasi-kelinci_20161002_162449.jpg',
-        lastName: 'Ghea',
-        firstName: 'Dieva',
-        email: 'dievaghealalala@gmail.com',
-        phone: '081234567890',
-        verified: true,
-    },
-    {
-        id: 2,
-        img: 'https://mmc.tirto.id/image/otf/500x0/2022/07/29/istock-1141673490_ratio-16x9.jpg',
-        lastName: 'Monica',
-        firstName: 'Diva',
-        email: 'dimonlalala@gmail.com',
-        phone: '081234560987',
-        verified: false,
-    },
-]
+  {
+    id: 1,
+    img: "https://asset-2.tstatic.net/lampung/foto/bank/images/ilustrasi-kelinci_20161002_162449.jpg",
+    lastName: "Ghea",
+    firstName: "Dieva",
+    email: "dievaghealalala@gmail.com",
+    phone: "081234567890",
+    verified: true,
+  },
+  {
+    id: 2,
+    img: "https://mmc.tirto.id/image/otf/500x0/2022/07/29/istock-1141673490_ratio-16x9.jpg",
+    lastName: "Monica",
+    firstName: "Diva",
+    email: "dimonlalala@gmail.com",
+    phone: "081234560987",
+    verified: false,
+  },
+];
 
 export const singleUser = [
+  {
     id: 1,
     title: "Novita Sari",
     img: "https://pergola-agava.si/wp-content/uploads/2020/08/profile-icon-png-898.png",
     info: {
-        username: "novita",
-        fullname: "novita",
-        email: "lalala@gmail.com",
+      username: "novita",
+      fullname: "novita",
+      email: "lalala@gmail.com",
     },
     activities: {
-        text: "belum ada",
-        time: "hari ini",
+      text: "belum ada",
+      time: "hari ini",
     },
+  },
 ];
